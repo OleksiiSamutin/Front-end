45a8722e8ce80f1b711b2a4314d0992cd02f6f14
OleksiiSamutin <samutin63@gmail.com>
2020-01-30 22:14:28 +0200

    Made an excersice from course Getting started with Java Script

--- front-end-masters/getting_started_with_js/exercises/three-pillars/ex.js
+++ front-end-masters/getting_started_with_js/exercises/three-pillars/ex.js
@@ -5,27 +5,47 @@ class Bookshelf {
 
 	// TODO: define methods `addFavoriteBook(..)`
 	// and `printFavoriteBooks()`
-}
+	addFavoriteBook(bookName) {
+		if (!bookName.includes("Great")) {
+			this.favoriteBooks.push(bookName);
+		}
+	}
 
+	printFavoriteBooks() {
+		console.log(`Favorite Books: ${this.favoriteBooks.length}`);
+		for (let bookName of this.favoriteBooks) {
+			console.log(String(bookName));
+		}
+	}
+}
+let myBookShelf = new Bookshelf()
+
+// Testing Bookshelf methods
+myBookShelf.addFavoriteBook("A Song of Ice and Fire");
+myBookShelf.addFavoriteBook("The Great Gatsby");
+myBookShelf.addFavoriteBook("Crime & Punishment");
+myBookShelf.addFavoriteBook("Great Expectations");
+myBookShelf.addFavoriteBook("You Don't Know JS");
+myBookShelf.printFavoriteBooks();
 function addFavoriteBook(bookName) {
 	if (!bookName.includes("Great")) {
 		favoriteBooks.push(bookName);
 	}
 }
 
-function printFavoriteBooks() {
-	console.log(`Favorite Books: ${favoriteBooks.length}`);
-	for (let bookName of favoriteBooks) {
-		console.log(bookName);
-	}
-}
 
-function loadBooks( /* .. */ ) {
-	// TODO: call fakeAjax( .. );
+function loadBooks(bookshelf) {
+	fakeAjax(BOOK_API, function onBooks(bookNames){
+		for (let bookName of bookNames){
+			bookshelf.addFavoriteBook(bookName);
+		}
+		bookshelf.printFavoriteBooks();
+	})
 }
 
 var BOOK_API = "https://some.url/api";
-
+var myBooks = new Bookshelf();
+loadBooks(myBooks);
 
 // ***********************
 
